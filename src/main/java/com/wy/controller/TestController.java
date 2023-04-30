3e3f14d06228128f0486f43b29d6a8a10f7e8157
wy <ousannjp@gmail.com>
2023-04-30 17:28:16 +0900

    update TestController.java

--- src/main/java/com/wy/controller/TestController.java
+++ src/main/java/com/wy/controller/TestController.java
@@ -4,14 +4,9 @@ package com.wy.controller;
 public class TestController {
 
     public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
+        System.out.println("ddd");
+        System.out.println("ddd");
+        System.out.println("ddd");
+        System.out.println("ddd");
     }
 }
