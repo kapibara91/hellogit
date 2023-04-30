c066211fbb30d76716961f3c934cd8ea0b75462a
butterfly <chinsannjp@gmail.com>
2023-04-30 16:53:32 +0900

    test 1 ----

--- src/main/java/com/wy/controller/Test1Controller.java
+++ src/main/java/com/wy/controller/Test1Controller.java
@@ -1,4 +1,7 @@
 package com.wy.controller;
 
 public class Test1Controller {
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/com/wy/controller/TestController.java
+++ src/main/java/com/wy/controller/TestController.java
@@ -4,6 +4,6 @@ package com.wy.controller;
 public class TestController {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("123");
     }
 }
