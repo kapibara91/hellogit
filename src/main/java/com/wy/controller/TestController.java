10a1e4fe4c2a0e457459900a79b95874e05c8cd5
wy <ousannjp@gmail.com>
2023-04-30 17:17:21 +0900

    modify Test1Controller.java

--- src/main/java/com/wy/controller/TestController.java
+++ src/main/java/com/wy/controller/TestController.java
@@ -5,5 +5,9 @@ public class TestController {
 
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
     }
 }
